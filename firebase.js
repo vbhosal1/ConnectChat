f87f5ffcf49657b1cd4c31e79291b8009ee88d37
Vinit Bhosale <vinitbhosale925@gmail.com>
2021-05-12 12:54:31 -0400

    firebase changes

--- firebase.js
+++ firebase.js
@@ -4,15 +4,14 @@ import firebase from 'firebase';
  * Connection of Firebase from client to database
  */
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD3MiaLOoFCxaSTA5l8bEjM4akyfE6U6y8",
-    authDomain: "connect-b3668.firebaseapp.com",
-    projectId: "connect-b3668",
-    storageBucket: "connect-b3668.appspot.com",
-    messagingSenderId: "360221709634",
-    appId: "1:360221709634:web:09a3ad2a66ec6c22aca4db",
-    measurementId: "G-4X4RGZX5Q1"
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyCaF-PrLklCwSPxepbzS53GrWCOCSXDuZE",
+    authDomain: "connectchat-b94b7.firebaseapp.com",
+    projectId: "connectchat-b94b7",
+    storageBucket: "connectchat-b94b7.appspot.com",
+    messagingSenderId: "1051639681124",
+    appId: "1:1051639681124:web:59efac14f3b5dc4f3a4f71"
+  };
   
 // Initialization of App for 1st time if not initialized on the first place
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
